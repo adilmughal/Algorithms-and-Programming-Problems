e654e71a070fe4d9888ab4b18b5f98dacde36dca
Adil Mughal <adil.mughal@live.com>
2012-11-20 23:34:28 +0500

    Created new test for Binary Search Tree
    BinarySearchTreePositiveTest

---
+++ ProgrammingProblems.Test/TreeProblemsTest.cs
@@ -0,0 +1,21 @@
+﻿using DataStructure;
+using NUnit.Framework;
+
+namespace ProgrammingProblems.Test
+{
+    [TestFixture]
+    public class TreeProblemsTest
+    {
+        [Test]
+        public void BinarySearchTreePositiveTest()
+        {
+            var bst = new BinarySearchTree<int, string>();
+            bst.Add(10, "Mr. and Mrs. Iqbal Ahmed Mughal");
+            bst.Add(1, "Sana");
+            bst.Add(2, "Mehwish");
+            bst.Add(3, "Adil");
+            bst.Add(4, "Ammara");
+            Assert.AreEqual("Ammara", bst.Retrieve(4));
+        }
+    }
+}
