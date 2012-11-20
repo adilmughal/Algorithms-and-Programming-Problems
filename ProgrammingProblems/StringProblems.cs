483a4788204e9b663ddc6f7443a198b8f619326d
Adil Mughal <adil.mughal@live.com>
2012-11-20 23:32:06 +0500

    Modified StringProblems class and HasUniqueCharacters method

--- ProgrammingProblems/StringProblems.cs
+++ ProgrammingProblems/StringProblems.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProgrammingProblems
 {
-    public class StringProblems
+    public static class StringProblems
     {
         public static bool HasUniqueCharacters(string input)
         {
+            //Assuming input supports ASCII encoding
             var bitSet = new bool[256];
 
             if (string.IsNullOrWhiteSpace(input))
@@ -13,9 +15,14 @@ namespace ProgrammingProblems
                 throw new ArgumentNullException("input");
             }
 
-            char[] strCharArray = input.ToCharArray();
+            /* OPTIMIZATION
+             * An ASCII encoded string since ASCII supports 256 chars unique max
+             * so any input with length greater than 256 must contains duplicate characters
+             */
+            if (input.Length > 256)
+                return false;
 
-            foreach (char c in strCharArray)
+            foreach (char c in input)
             {
                 int value = Convert.ToInt32(c);
                 if (bitSet[value])
@@ -24,6 +31,24 @@ namespace ProgrammingProblems
                 }
                 bitSet[value] = true;
             }
+            return true;
+        }
+
+        public static bool HasUniqueUnicodeCharacters(string input)
+        {
+            //This would do for Unicode string as well
+            if (string.IsNullOrWhiteSpace(input))
+                throw new ArgumentNullException("input");
+
+            var hashSet = new HashSet<char>();
+
+            foreach (char c in input)
+            {
+                if (hashSet.Contains(c))
+                    return false;
+                hashSet.Add(c);
+            }
+
             return true;
         }
     }
