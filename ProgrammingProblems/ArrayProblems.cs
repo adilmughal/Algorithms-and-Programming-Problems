1d9973f1e30618c22123a7c25c73661c864f6072
Adil Mughal <adil.mughal@live.com>
2012-11-20 23:36:42 +0500

    Added FindDuplicateItems implementation

--- ProgrammingProblems/ArrayProblems.cs
+++ ProgrammingProblems/ArrayProblems.cs
@@ -25,6 +25,59 @@ namespace ProgrammingProblems
             return maxSum;
         }
 
+        public static IEnumerable<TSource> FindDuplicateItems<TSource>(IEnumerable<TSource> input)
+        {
+            if (input == null)
+                throw new ArgumentNullException("input");
+
+            Dictionary<TSource, int> dictionary = new Dictionary<TSource, int>();
+
+            foreach (TSource item in input)
+                if (dictionary.ContainsKey(item))
+                    dictionary[item] = dictionary[item] + 1;
+                else
+                    dictionary.Add(item, 1);
+
+            foreach (KeyValuePair<TSource, int> entry in dictionary)
+                if (entry.Value > 1)
+                    yield return entry.Key;
+        }
+
+
+        public static int FindSingleDuplicateUsingSum(int[] input)
+        {
+            if (input == null)
+                throw new ArgumentNullException("input");
+
+            int n = input.Length;
+            int sum = n*(n - 1)/2;
+            int actualSum = 0;
+
+            for (int index = 0; index < input.Length; index++)
+                actualSum += input[index];
+
+            return Math.Abs(actualSum - sum);
+        }
+
+        public static int FindSingleDuplicate(int[] input)
+        {
+            if (input == null)
+                throw new ArgumentNullException("input");
+
+            int index = 0;
+            int xorValue = 0;
+
+            while (index < input.Length)
+                xorValue = xorValue ^ input[index++];
+
+            index = 0;
+
+            while (index < input.Length -1)
+                xorValue = xorValue ^ input[index++];
+
+            return input[xorValue-1];
+        }
+
         public static IEnumerable<TSource> IsIntersection<TSource>(IEnumerable<TSource> first, IEnumerable<TSource> second)
         {
             if (first == null)
