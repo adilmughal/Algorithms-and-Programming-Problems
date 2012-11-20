06d8720626f54f6b2e4823784dc0d01cd888ba44
Adil Mughal <adil.mughal@live.com>
2012-11-20 23:30:30 +0500

    Added Unit test for bitwise problems

---
+++ ProgrammingProblems.Test/BitwiseProblemsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ProgrammingProblems.Test
+{
+    [TestFixture]
+    public class BitwiseProblemsTest
+    {
+        [Test]
+        public void ConvertToBase10PositiveTest()
+        {
+            byte input = (byte)2;
+            Assert.AreEqual(2, BitwiseProblems.ConvertToBase10(input));
+
+        }
+        
+        
+    }
+}
