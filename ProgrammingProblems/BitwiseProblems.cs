cbd0cb5948a0464bd0c88beb948c974c05355c24
Adil Mughal <adil.mughal@live.com>
2012-11-20 23:30:15 +0500

    Added BitwiseProblems Class and ConvertToBase10 method

---
+++ ProgrammingProblems/BitwiseProblems.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgrammingProblems
+{
+    public class BitwiseProblems
+    {
+        public static double ConvertToBase10(byte input)
+        {
+            int power = 0;
+            double result = 0;
+            while (input !=0)
+            {
+                int bitValue = input & 1;
+                result = result + Math.Pow(2, power)*bitValue;
+                power++;
+                input = (byte) (input >> 1); 
+            }
+
+            return result;
+        }
+    }
+}
