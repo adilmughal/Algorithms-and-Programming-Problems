ae6bcfe8994c3d7a0e7cc2d6d4b93c7c7ba538c1
Adil Mughal <adil.mughal@live.com>
2013-02-15 21:58:21 +0500

    Added ReplaceSpace and CountCompression Test

--- ProgrammingProblems.Test/StringProblemsTest.cs
+++ ProgrammingProblems.Test/StringProblemsTest.cs
@@ -12,5 +12,36 @@ namespace ProgrammingProblems.Test
             bool result = StringProblems.HasUniqueCharacters(input);
             Assert.AreEqual(false, result);
         }
+
+
+        [Test]
+        public void ReplaceSpacePositiveTest()
+        {
+            char[] input = "Adil Mughal".ToCharArray();
+            char[] expectedOutput = "Adil%20Mughal".ToCharArray();
+            Assert.AreEqual(expectedOutput, StringProblems.ReplaceSpace(input));
+
+
+            input = "Adil Mughal ".ToCharArray();
+            expectedOutput = "Adil%20Mughal%20".ToCharArray();
+            Assert.AreEqual(expectedOutput, StringProblems.ReplaceSpace(input));
+        }
+
+        [Test]
+        public void PerformCountCompressionPositiveTest()
+        {
+            string input = "aabcccaaa";
+            string expectedOutput = "a2b1c3a3";
+            Assert.AreEqual(expectedOutput, StringProblems.PerformCountCompression(input));
+
+            input = "aabcdd";
+            expectedOutput = "a2b1c1d2";
+            Assert.AreEqual(expectedOutput, StringProblems.PerformCountCompression(input));
+
+            input = "abcd";
+            expectedOutput = "abcd";
+            Assert.AreEqual(expectedOutput, StringProblems.PerformCountCompression(input));
+
+        }
     }
 }
