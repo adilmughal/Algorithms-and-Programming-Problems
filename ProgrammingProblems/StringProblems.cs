b10407445b351e41cddbc82b7f5a5e0c1c12abf1
Adil Mughal <adil.mughal@live.com>
2013-02-15 21:58:04 +0500

    Added solution to two more problems
    Added ReplaceSpace and CountCompression

--- ProgrammingProblems/StringProblems.cs
+++ ProgrammingProblems/StringProblems.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace ProgrammingProblems
 {
@@ -16,7 +18,7 @@ namespace ProgrammingProblems
             }
 
             /* OPTIMIZATION
-             * An ASCII encoded string since ASCII supports 256 chars unique max
+             * An ASCII encoded string since ASCII supports 0-255 chars unique max
              * so any input with length greater than 256 must contains duplicate characters
              */
             if (input.Length > 256)
@@ -50,6 +52,60 @@ namespace ProgrammingProblems
             }
 
             return true;
+        }
+
+        public static char[] ReplaceSpace(char[] input)
+        {
+            /* Replaces space ' ' in char[] with %20 */
+
+            int spaceCount = input.Count(c => c == ' ');
+            int newLength = input.Length + spaceCount * 2;
+            char[] output = new char[newLength];
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                if (input[i] == ' ')
+                {
+                    output[newLength - 1] = '0';
+                    output[newLength - 2] = '2';
+                    output[newLength - 3] = '%';
+                    newLength -= 3;
+                }
+                else
+                {
+                    output[newLength - 1] = input[i];
+                    newLength -= 1;
+                }
+            }
+            return output;
+        }
+
+        public static string PerformCountCompression(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                throw new ArgumentException("input");
+
+            if (HasUniqueCharacters(input))
+                return input;
+
+            if (input.Length == 1)
+                return input;
+
+            int currentChar = 0;
+            StringBuilder output=new StringBuilder();
+
+            while (currentChar < input.Length)
+            {
+                int counter = 1;
+                output.Append(input[currentChar]);
+                while ( (currentChar+counter) < input.Length && 
+                    input[currentChar] == input[currentChar + counter])
+                    counter++;
+                output.Append(counter);
+                currentChar += counter;
+            }
+
+            return output.ToString();
         }
     }
 }
