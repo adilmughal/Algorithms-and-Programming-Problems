876d44113bcd407647f0a6e7514ea6da591b65af
Adil Mughal <adil.mughal@live.com>
2013-04-14 13:13:54 +0500

    Added DataStructure.Test project
    New project to test data structures.
    Added PriorityQueueTest and BinaryHeapTest

---
+++ DataStructure.Test/BinaryHeapTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace DataStructure.Test
+{
+    [TestFixture]
+    public class BinaryHeapTest
+    {
+        [Test]
+        public void BinaryHeapDoubleDeleteTest()
+        {
+            BinaryHeap<int> binaryHeap = new BinaryHeap<int>(10);
+            binaryHeap.Insert(4);
+            binaryHeap.Insert(1);
+            binaryHeap.Insert(3);
+            binaryHeap.Insert(8);
+            binaryHeap.Insert(0);
+            Assert.AreEqual(8, binaryHeap.DeleteMax());
+            Assert.AreEqual(4, binaryHeap.DeleteMax());
+        }
+
+        [Test]
+        public void BinaryHeapInsertDeleteInsertTest()
+        {
+            BinaryHeap<int> binaryHeap = new BinaryHeap<int>(10);
+            binaryHeap.Insert(4);
+            binaryHeap.Insert(6);
+            binaryHeap.Insert(3);
+            binaryHeap.Insert(8);
+            Assert.AreEqual(8, binaryHeap.DeleteMax());
+            binaryHeap.Insert(10);
+            Assert.AreEqual(10, binaryHeap.DeleteMax());
+        }
+
+        [Test]
+        public void BinaryHeapSimpleTest()
+        {
+            BinaryHeap<int> binaryHeap = new BinaryHeap<int>(10);
+            binaryHeap.Insert(4);
+            binaryHeap.Insert(6);
+            binaryHeap.Insert(3);
+            binaryHeap.Insert(8);
+            Assert.AreEqual(8, binaryHeap.DeleteMax());
+        }
+    }
+}
---
+++ DataStructure.Test/PriorityQueueTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace DataStructure.Test
+{
+    [TestFixture]
+    public class PriorityQueueTest
+    {
+        [Test]
+        public void PriorityQueueDoubleDeleteTest()
+        {
+            PriorityQueue<int> pQueue = new PriorityQueue<int>(10);
+            pQueue.Insert(4);
+            pQueue.Insert(1);
+            pQueue.Insert(3);
+            pQueue.Insert(8);
+            pQueue.Insert(0);
+            Assert.AreEqual(0, pQueue.DeleteMin());
+            Assert.AreEqual(1, pQueue.DeleteMin());
+        }
+
+        [Test]
+        public void PriorityQueueSimpleTest()
+        {
+            PriorityQueue<int> pQueue = new PriorityQueue<int>(10);
+            pQueue.Insert(4);
+            pQueue.Insert(6);
+            pQueue.Insert(3);
+            pQueue.Insert(8);
+            Assert.AreEqual(3, pQueue.DeleteMin());
+        }
+    }
+}
