39a3716794b5168c40f5fcc4714636020e1ff7ad
Adil Mughal <adil.mughal@live.com>
2013-04-14 13:11:15 +0500

    Added Common Project
    A library to keep common and helper methods

---
+++ Common/Helper.cs
@@ -0,0 +1,12 @@
+﻿namespace Common
+{
+    public class Helper
+    {
+        public static void Swap<T>(T[] input, int first, int second)
+        {
+            T temp = input[first];
+            input[first] = input[second];
+            input[second] = temp;
+        }
+    }
+}
